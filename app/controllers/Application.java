3996b0052cfbd90c7662d4b88421b9fb4233eee7
Rafael Veras <rafaeldotveras@gmail.com>
2015-04-30 02:18:27 -0400

    Added simple session tracking with play.Cache
    After login, the user's username is stored in the session
    then put into cache. Currently the main page prints out the
    currently logged in user to test. This test first checks if the
    current session has a uuid field, and if so, retrieves the username
    from cache.
    
    TODO: Create utility function, or maybe ControllerCache class
    to deal with caching and sessions

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,6 @@
 package controllers;
 
-import play.*;
+import play.cache.Cache;
 import play.mvc.*;
 
 import views.html.*;
@@ -8,6 +8,12 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
+        // test, get uuid from cache
+        String uuid = session("uuid");
+        if (uuid != null) {
+            String currentUser = (String)Cache.get(uuid+"username");
+            System.out.println("Current user: " + currentUser);
+        }
         return ok(index.render("GreatDane BookStore."));
     }
 
