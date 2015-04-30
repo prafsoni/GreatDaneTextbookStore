16d162d1970f5dbc0a30d89e5c4ce5f7aecf1fc4
Rafael Veras <rafaeldotveras@gmail.com>
2015-04-30 00:26:15 -0400

    Fixed login form and refactored controllers
    The login form was checking 'pwd' instead of 'password',
    causing the user's login request to never go through. I
    have also make the UserOperations class have mostly static
    functions as the class has no instance variables and the functions
    are pure against the database connection.
    
    Many comments have been added as well.

--- app/models/UserOperations.java
+++ app/models/UserOperations.java
@@ -19,19 +19,26 @@ import java.util.Arrays;
  */
 public class UserOperations extends Model {
 
-    private com.mongodb.async.client.MongoDatabase getdatabaseasync(){
+    private static com.mongodb.async.client.MongoDatabase getdatabaseasync(){
         MongoClient mongoClient = MongoClients.create();
         com.mongodb.async.client.MongoDatabase mongoDatabase = mongoClient.getDatabase("BookStore");
         return mongoDatabase;
     }
 
-    private com.mongodb.client.MongoDatabase getdatabase(){
+    private static com.mongodb.client.MongoDatabase getdatabase(){
         com.mongodb.MongoClient mongoClient = new com.mongodb.MongoClient();
         com.mongodb.client.MongoDatabase mongoDatabase = mongoClient.getDatabase("BookStore");
         return mongoDatabase;
     }
 
-    public Boolean createuser(Users user ){
+    /**
+     * Creates a user in the DB from a User instance
+     * @param user The User instance used to construct a user document in the DB.
+     *             This instance must have a valid field for: lname,fname,uname,email,password,mod,cDate,
+     *             role, and status
+     * @return True if the user was inserted, False otherwise.
+     */
+    public static Boolean createuser(Users user ){
         try {
             Document doc = new Document("fname", user.fname)
                     .append("lname", user.lname)
@@ -45,23 +52,22 @@ public class UserOperations extends Model {
                     .append("status", user.status);
             com.mongodb.async.client.MongoDatabase database = getdatabaseasync();
             MongoCollection<Document> collection = database.getCollection("Users");
-            collection.insertOne(doc, (Void result, final Throwable t) -> System.out.println(user.id));
+            collection.insertOne(doc, (Void result, final Throwable t) -> System.out.println(user));
             return true;
-        }catch(Exception ex){
+        }catch(Exception ex){ // TODO catch proper exceptions
+            System.out.println(ex);
             return false;
         }
     }
 
-    public Boolean checkuserpass(String uname, String pwd){
+    public static Boolean checkuserpass(String uname, String pwd){
         com.mongodb.client.MongoDatabase database = getdatabase();
         com.mongodb.client.MongoCollection<Document> collection = database.getCollection("Users");
 
+        System.out.println("Checking user: " + uname + " with entered pass: " + pwd);
+
         Document result = collection.find(and(eq("uname", uname), eq("password", pwd))).first();
-        if(result!=null){
-            return true;
-        }
-        else {
-            return false;
-        }
+
+        return result == null ? false : true;
     }
 }
